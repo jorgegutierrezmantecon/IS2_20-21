e0713e63794f578c1d44ee32d1c6a9f7679255b8
Jorge Gutiérrez Mantecón <jgm188@alumnos.unican.es>
2021-05-19 12:01:03 +0200

    Actualización SONAR

--- practica5refactorizada/src/main/java/es/unican/is2/practica5refac/saldoInsuficienteException.java
+++
@@ -1,9 +0,0 @@
-package es.unican.is2.practica5refac;
-
-@SuppressWarnings("serial")
-public class saldoInsuficienteException extends RuntimeException {
-
-	public saldoInsuficienteException (String mensaje) {
-		super(mensaje);
-	}
-}
